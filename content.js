03d6344707f045bb511f3ec89a2e91a2ad9c4fbd
Darkmecha <98684156+darkmechanism@users.noreply.github.com>
2022-02-04 11:20:20 +0300

    Add files via upload

---
+++ content.js
@@ -0,0 +1,15 @@
+Element.prototype.appendAfter = function (element) {
+    element.parentNode.insertBefore(this, element.nextSibling);
+  },false;
+
+var buttonsuperior = document.getElementsByClassName("btn_buynow_market")[0].parentElement.parentElement.parentElement.parentElement;
+
+var htmlcode = '<td colspan="2" class="infobox-data" style="text-align:center"><div class="plainlinks btn_wrapper" style="width:100%"><a rel="nofollow" class="external text" href="https://backpack.tf/stats/Unique/$ITEM_NAME/Tradable/Craftable" target="_blank"><span class="btn_buynow_backpack">View in backpack.tf<span></span></span></a></div>\n</td>';
+
+var itemname = window.location.href.split("wiki.teamfortress.com/wiki/")[1].split("/")[0].replace("_", "%20");
+
+var newelement = document.createElement("tr");
+
+newelement.innerHTML = htmlcode.replace("$ITEM_NAME", itemname);
+
+newelement.appendAfter(buttonsuperior);
