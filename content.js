cc12311fb90bc280cdc5bcccb18f399b503c52a6
Darkmecha <98684156+darkmechanism@users.noreply.github.com>
2022-02-04 15:39:36 +0300

    Update content.js
    small fix

--- content.js
+++ content.js
@@ -6,7 +6,7 @@ var buttonsuperior = document.getElementsByClassName("btn_buynow_market")[0].par
 
 var htmlcode = '<td colspan="2" class="infobox-data" style="text-align:center"><div class="plainlinks btn_wrapper" style="width:100%"><a rel="nofollow" class="external text" href="https://backpack.tf/stats/Unique/$ITEM_NAME/Tradable/Craftable" target="_blank"><span class="btn_buynow_backpack">View in backpack.tf<span></span></span></a></div>\n</td>';
 
-var itemname = window.location.href.split("wiki.teamfortress.com/wiki/")[1].split("/")[0].replace("_", "%20");
+var itemname = window.location.href.split("wiki.teamfortress.com/wiki/")[1].split("/")[0].replace(/ /g, "%20");
 
 var newelement = document.createElement("tr");
 
